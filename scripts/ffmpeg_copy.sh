ba4d4fe66b6970a2072eb6d899ea667948d76eb5
Masud <masudhasan@google.com>
2020-06-28 17:55:11 -0400

    changed script to add env variable

--- scripts/ffmpeg_copy.sh
+++ scripts/ffmpeg_copy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2019 Google Inc.
+# Copyright 2020 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,6 @@
 #!/bin/bash
 for file in *.mp4
 do
-ffmpeg -i "$file" -codec:a aac  -ac 2  -ar 48k -c copy -f segment -segment_format mpegts -segment_time 5 "${fil>
+ffmpeg -i "$file" -codec:a aac  -ac 2  -ar 48k -c copy -f segment -segment_format mpegts  -segment_time 5 "${file%.*}~"%1d.mp4
 done
-gsutil -m cp *~*.mp4 gs://drone-video-dataset/
+gsutil -m cp *~*.mp4 gs://${VIDEO_CLIPS_BUCKET}/
