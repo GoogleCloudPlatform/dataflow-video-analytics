9fcd93e0c7648eb14403b7250041168e2acfebb6
Masud <masudhasan@google.com>
2020-09-14 10:11:41 -0400

    ffmpeg script - two steps

--- scripts/ffmpeg_copy.sh
+++ scripts/ffmpeg_copy.sh
@@ -12,8 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
+rm -r *.mp4
+gsutil -m cp $1 . 
 for file in *.mp4
 do
-ffmpeg -i "$file" -codec:a aac  -ac 2  -ar 48k -c copy -movflags faststart -f segment -segment_format mpegts  -segment_time 5 "${file%.*}~"%1d.mp4
+ffmpeg -i "$file" -c:v libx264 -crf 22 -map 0 -segment_time 3 -reset_timestamps 0 -g 30 -sc_threshold 0 -force_key_frames "expr:gte(t,n_forced*1)>
+ for split_file in *~*.mp4
+ do
+   ffmpeg -i "$split_file" -c copy -map 0 -movflags faststart "modified_$split_file"
+ done
 done
-gsutil -m cp *~*.mp4 $1
+gsutil -m cp *modified_*.mp4 $2
